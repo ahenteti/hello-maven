fb30efa7ff922288567e736d03e2d53d23d356e4
ahenteti <ahmed.henteti@capgemini.com>
2021-02-26 17:14:56 +0100

    add HelloMaven class

---
+++ src/main/java/com/github/ahenteti/hellomaven/hellomaven/HelloMaven.java
@@ -0,0 +1,8 @@
+package com.github.ahenteti.hellomaven.hellomaven;
+
+public class HelloMaven {
+
+    public static void hello() {
+        System.out.println("Hello Maven Central Repository!");
+    }
+}
---
+++ src/test/java/com/github/ahenteti/hellomaven/HelloMavenTest.java
@@ -0,0 +1,13 @@
+package com.github.ahenteti.hellomaven;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloMavenTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
