21276bcf846e979389d07d3052367bd8f8044327
ahenteti <ahmad.henteti@gmail.com>
2021-03-02 20:40:26 +0100

    version 1.3

--- src/main/java/com/github/ahenteti/hellomaven/HelloMaven.java
+++ src/main/java/com/github/ahenteti/hellomaven/HelloMaven.java
@@ -3,6 +3,6 @@ package com.github.ahenteti.hellomaven;
 public class HelloMaven {
 
     public static void hello() {
-        System.out.println("Hello Maven Central Repository!");
+        System.out.println("Hello Maven Central Repository v1.3!");
     }
 }
